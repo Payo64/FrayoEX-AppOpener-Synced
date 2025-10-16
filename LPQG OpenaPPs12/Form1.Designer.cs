6f00d3b287a7712fa1966aa27b2d68083f925c42
Payo64 <Kingfishsecond@gmail.com>
2025-10-16 14:41:12 +0700

    Add project files.

---
+++ LPQG OpenaPPs12/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace LPQG_OpenaPPs12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(171, 227);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Launch Option";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(654, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(280, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 46);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Apps";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(322, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(288, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 60);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Open Selected App";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(280, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 46);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(307, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "App Selected";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(344, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "WARNING:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(195, 397);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(390, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "IF THE SELECTED APP ASKED FOR ADMINISTRATOR PERMISSION,";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(209, 415);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(346, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "PLEASE CLICK \"YES\" OR THIS APP WILL THROW AN ERROR";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(322, 280);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(145, 29);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Administrator";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FrayoEX\'s App Opener";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ LPQG OpenaPPs12/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel.Design;
+
+namespace LPQG_OpenaPPs12
+{
+
+    public partial class Form1 : Form
+    {
+        private string selectedExePath = string.Empty;
+        private string selectedapp = string.Empty;
+        private string Argumentation = string.Empty;
+        private List<bool> checklist = new List<bool>() { false, false, false };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Hide();
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checklist[1] = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Argumentation = textBox1.Text;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog op = new OpenFileDialog() { Filter = "Windows EXE|*.exe" })
+            {
+                
+                
+
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    selectedExePath = op.FileName;
+                    selectedapp = Path.GetFileName(op.FileName);  // store the file path
+                    string exe;
+                    exe = op.FileName;
+                    
+                    label4.Show();
+                    { label4.Text = selectedapp; label4.Refresh(); }
+                    MessageBox.Show(" App Selected!",
+                                    "Open", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    
+
+                }
+            }
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = selectedExePath;
+                startInfo.Arguments = Argumentation;
+                startInfo.WorkingDirectory = Path.GetDirectoryName(selectedExePath);
+
+                // run as admin
+                startInfo.UseShellExecute = true;
+                startInfo.Verb = "runas";
+
+                try
+                {
+                    Process.Start(startInfo);
+                    MessageBox.Show("App started as Administrator with arguments: " + Argumentation,
+                                    "LQPG App Opener", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Win32Exception)
+                {
+                    MessageBox.Show("Administrator privileges were denied.",
+                                    "LQPG App Opener", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                
+
+            }
+
+            else 
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = selectedExePath;
+                startInfo.Arguments = Argumentation;
+                startInfo.WorkingDirectory = Path.GetDirectoryName(selectedExePath);
+
+                // run as admin
+                startInfo.UseShellExecute = false;
+                startInfo.Verb = "runas";
+
+                try
+                {
+                    Process.Start(startInfo);
+                    MessageBox.Show("App started as Administrator with arguments: " + Argumentation,
+                                    "LQPG App Opener", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Win32Exception)
+                {
+                    
+                }
+            }
+
+            
+
+
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
